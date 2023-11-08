33e100e55ac16f05fe4266451c556fad2ce5b469
alpinmohsa541 <alpinmohsa@gmail.com>
2023-11-08 18:53:45 +0700

    Create activity login, register, add firebase, and auth login, register

---
+++ app/src/main/java/com/example/projectdns/SignInActivity.java
@@ -0,0 +1,66 @@
+//package com.example.myrakamin
+//
+//import android.content.Intent
+//import android.os.Bundle
+//import android.widget.Toast
+//import androidx.appcompat.app.AppCompatActivity
+////import com.example.myrakamin.SignUpActivity
+//import com.example.projectdns.databinding.ActivitySignInBinding
+//import com.google.firebase.auth.FirebaseAuth
+//
+//class SignInActivity : AppCompatActivity() {
+//
+//private lateinit var binding: ActivitySignInBinding
+//private lateinit var firebaseAuth: FirebaseAuth
+//
+//        override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = ActivitySignInBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        firebaseAuth = FirebaseAuth.getInstance()
+//
+//        binding.registerLink.setOnClickListener {
+//        val intent = Intent(this, SignUpActivity::class.java)
+//        startActivity(intent)
+//        }
+//
+//        binding.button.setOnClickListener {
+//        val email = binding.emailEt.text.toString()
+//        val pass = binding.passET.text.toString()
+//
+//        if (email.isNotEmpty() && pass.isNotEmpty()) {
+//        firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener { task ->
+//        if (task.isSuccessful) {
+//        val intent = Intent(this, CekSaldoActivity::class.java)
+//        startActivity(intent)
+//        } else {
+//        val exception = task.exception
+//        when {
+//        exception?.message?.contains("password") == true -> {
+//        Toast.makeText(this, "Wrong password", Toast.LENGTH_SHORT).show()
+//        }
+//        exception?.message?.contains("user record") == true -> {
+//        Toast.makeText(this, "User not registered", Toast.LENGTH_SHORT).show()
+//        }
+//        else -> {
+//        Toast.makeText(this, "Authentication failed: ${exception?.message}", Toast.LENGTH_SHORT).show()
+//        }
+//        }
+//        }
+//        }
+//        } else {
+//        Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+//        }
+//        }
+//        }
+//        }
+//
+////    override fun onStart() {
+////        super.onStart()
+////
+////        if(firebaseAuth.currentUser != null){
+////            val intent = Intent(this, MainActivity::class.java)
+////            startActivity(intent)
+////        }
+////    }
